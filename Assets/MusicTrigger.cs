a4d211627c5ac3075c7d90cce829351696accf24
Rusty Bailey <rustylbailey@gmail.com>
2021-03-02 09:31:13 -0500

    Piggyback off of the music trigger to experiment with a fading background

--- Assets/MusicTrigger.cs
+++ Assets/MusicTrigger.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicTrigger : MonoBehaviour
 {
     [SerializeField] string track1;
     [SerializeField] string track2;
+    [SerializeField] Image backgroundImage;
+    [SerializeField] Color color1;
+    [SerializeField] Color color2;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
             PlayDesiredMusic();
+            StartCoroutine(FadeToDesiredColor());
         }
     }
 
@@ -33,5 +38,19 @@ public class MusicTrigger : MonoBehaviour
         string fadeOutTrack = currentTrack.GetName() == track1 ? track1 : track2;
         string fadeInTrack = currentTrack.GetName() == track1 ? track2 : track1;
         AudioManager.instance.CrossFadeBetweenTwoTracks(fadeOutTrack, fadeInTrack, 1f);
+    }
+
+    private IEnumerator FadeToDesiredColor()
+    {
+        float elapsedTime = 0f;
+        float totalTime = 2f;
+        Color currentColor = backgroundImage.color;
+
+        while (elapsedTime < totalTime)
+        {
+            backgroundImage.color = Color.Lerp(currentColor, color2, (elapsedTime / totalTime));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
