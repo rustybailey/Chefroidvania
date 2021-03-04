6a5e894840e3f51528b38ad88b7717d27b9cb520
Rusty Bailey <rustylbailey@gmail.com>
2021-03-03 22:58:43 -0500

    Move game object above player's hand; add TODO comments

--- Assets/Scripts/GetItemTrigger.cs
+++ Assets/Scripts/GetItemTrigger.cs
@@ -9,7 +9,24 @@ public class GetItemTrigger : MonoBehaviour
         Player player = collision.GetComponent<Player>();
         if (player && !player.StateMachine.CurrentState.Equals(player.getItemState))
         {
+            // TODO: Do all of this in a coroutine
+            // TODO: Wait until the player is grounded before triggering the state change?
+            // TODO: Create a getItemIdleState so that this animation can last longer?
+            // The idle anim could be the sparkling part of the current anim looping (the sfx lasts 4-5s)
+
             player.StateMachine.ChangeState(player.getItemState);
+            
+            // Move object above the player's hand
+            gameObject.transform.position = new Vector2(
+                collision.transform.position.x + (1f * player.FacingDirection),
+                collision.transform.position.y + 1f
+            );
+
+            // TODO: Transition to a zoomed in the camera
+            // TODO: Add object to player's inventory and/or game state
+            // TODO: How do we know what the object is? Pass the gameObject name to the game state method?
+
+            // TODO: Destroy game object
         }
     }
 }
