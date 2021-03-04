2560a6026ea3cc9bb4c00c485415341c47140805
Rusty Bailey <rustylbailey@gmail.com>
2021-03-03 22:22:06 -0500

    Set up getItem state and animation

---
+++ Assets/Scripts/Player/States/PlayerGetItemState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGetItemState : PlayerState
+{
+    public PlayerGetItemState(Player player, string animationBooleanName) : base(player, animationBooleanName)
+    {
+    }
+}
